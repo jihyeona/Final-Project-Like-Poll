19f9e52feba2836f2740e6e3518015aa55096f8e
emelie Lönnerheden <you@example.com>
2020-06-15 14:25:46 +0200

    cleaning code

--- frontend/src/components/LikeList.js
+++ frontend/src/components/LikeList.js
@@ -1,19 +1,13 @@
 import React from 'react'
-import { ImgWrapper, LinkText } from '../lib/images'
-import { SmCard } from 'components/SmCard'
 
 export const LikeList = () => {
   // shows the list of things you’ve liked
   // SmallImages
   // stretch goal: when hovered, show the name of the image
   // stretch goal: when clicked, it redirects you to the poll page
-  
   return (
-    <ImgWrapper>
-     <LinkText >TEXT title</LinkText>
-     <SmCard/>
-    </ImgWrapper>
-  )
-}
+    <></>
+  )}
+ 
 
 export default LikeList
